9419c51f58bda2f98a86958baa1dcb10746fc3d4
Debra Ansell <38991128+BrightWearables@users.noreply.github.com>
2019-10-25 14:36:58 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
+// % color=#3CCBB1 icon="\uf185"
 namespace TCS34725 {
 
     /**
-     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
-     * @param interval speed of scroll; eg: 150, 100, 200, -100
+     * Read color
      */
-    //% blockId=tcs34725_get_color="readColor" blockGap=8
-    //% async
-    //% parts="ledmatrix" interval.defl=150
+    //% blockId=tcs34725_get_color block="readColor"
     export function readcolor() : number {
         return 0;
     }
 }
-
 basic.forever(function () {
 	
 })
